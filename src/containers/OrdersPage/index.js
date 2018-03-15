373de9d4a904444627fb9bedb2af7ad77f3e3d61
Eduardo <drdpedroso@gmail.com>
2018-03-14 22:31:05 -0300

    Adding blueprint

--- src/containers/OrdersPage/index.js
+++ src/containers/OrdersPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Intent, Spinner, DatePickerFactory } from "@blueprintjs/core";
 
 const OrdersPage = () => {
   return (
     <div>
+    <Spinner intent={Intent.PRIMARY} />
       <p>ALGUMA COISA!</p>
     </div>
   )
